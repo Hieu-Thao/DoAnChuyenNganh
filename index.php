d466efa5471cbd03ad2416dc62efb48d9d912e28
HiThaoz <114085233+Hieu-Thao@users.noreply.github.com>
2023-11-22 21:20:45 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <title>Document</title>
+    <style>
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 95vh;
+        }
+    </style>
+</head>
+<body>
+    <div class="login_box">
+        <img src="2.png">
+        <h2>QUẢN LÝ CÔNG TÁC COI THI </br> KHOA KỸ THUẬT VÀ CÔNG NGHỆ</h2>
+        <div class="login_input">
+            <input type="text" placeholder="Email"/>
+            <input type="password" placeholder="Mật khẩu"/>
+            <div>
+                <input id="remember_pw" type="checkbox"/>
+                <label for="remember_pw">Nhớ mật khẩu</label>
+            </div>
+            <button>Đăng nhập</button>
+        </div>
+
+    </div>
+</body>
+</html>
